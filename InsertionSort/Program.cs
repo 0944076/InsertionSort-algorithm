1c136450c5b157a104e577844a146fe1cf14199d
Ibrahim El Hamdioui <31695103+0944076@users.noreply.github.com>
2019-03-07 20:48:13 +0100

    Add files via upload

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace InsertionSort{
+
+
+    public class inserionSorting
+    {
+        public static void Sort(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n; ++i)
+            {
+                int key = array[i];
+                int j = i - 1;
+
+                while(j >= 0 && array[j] > key)
+                {
+                    array[j + 1] = array[j];
+                    j = j - 1;
+                }
+                array[j + 1] = key;
+            }
+        }
+
+        public static void PrintArray(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n; i++)
+            {
+                System.Console.WriteLine(array[i] + " ");
+            }
+        }
+
+        public static void Main()
+        {
+            int[] array = { 14, 58, 64, 877, 47};
+            
+            inserionSorting.Sort(array);
+            PrintArray(array);
+        }
+    }
+}
+
+
